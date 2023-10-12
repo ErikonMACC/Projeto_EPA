ab2c4949bb4643bf4f553dbcd7ff15a4ff066623
ErikonMACC <erik.roblox0602@gmail.com>
2023-10-12 13:33:37 -0300

    Correções de Bugs

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,6 +1,7 @@
 let score_val = document.querySelector('.score_val');
 let score_title = document.querySelector('.score_title');
 let frames = 0;
+let die = 0;
 
 const knight = document.querySelector('.knight')
 const castle = document.querySelector('.castle')
@@ -11,15 +12,18 @@ const pointing = document.querySelector('.point')
 const jumping = document.querySelector('.jump')
 
 const jump = () => {
-    knight.classList.add('jump')
-    jumping.play();
-
-    setTimeout(() => {
-
-        knight.classList.remove('jump')      
+    if (die != 1) {
         
-    }, 800)
-    
+        knight.classList.add('jump')
+        jumping.play();
+        
+
+        setTimeout(() => {
+
+            knight.classList.remove('jump')
+
+        }, 800)
+    }
 }
 
 document.addEventListener('keydown', jump);
@@ -32,42 +36,42 @@ const loop = setInterval(() => {
     let intervaloDeFrames = 10;
     let passouOIntervalo = frames % intervaloDeFrames === 0;
 
-    const floor1Position =  +window.getComputedStyle(floor1).right.replace('px', '');
-    const floor2Position =  +window.getComputedStyle(floor2).right.replace('px', '');
+    const floor1Position = +window.getComputedStyle(floor1).right.replace('px', '');
+    const floor2Position = +window.getComputedStyle(floor2).right.replace('px', '');
 
-    const castlePosition =  castle.offsetLeft;
+    const castlePosition = castle.offsetLeft;
     const knightPosition = +window.getComputedStyle(knight).bottom.replace('px', '');
 
-    if(castlePosition <= 220 && castlePosition > 0 && knightPosition < 130) {
+    if (castlePosition <= 220 && castlePosition > 0 && knightPosition < 130) {
 
-        castle.style.animation = 'none' ;
-        castle.style.left = `${castlePosition}px` ;
+        castle.style.animation = 'none';
+        castle.style.left = `${castlePosition}px`;
 
-        knight.style.animation = 'none' ;
-        knight.style.bottom = `${knightPosition}px` ;
+        knight.style.animation = 'none';
+        knight.style.bottom = `${knightPosition}px`;
 
-        knight.src = 'images/Death.png' ;
-        knight.style.width = '270px' ; 
-        knight.style.marginLeft = '-100px' ;
-        knight.style.marginBottom = '-25px' ;
-        knight.style.bottom = '0' ;
+        knight.src = 'images/Death.png';
+        knight.style.width = '270px';
+        knight.style.marginLeft = '-100px';
+        knight.style.marginBottom = '-25px';
+        knight.style.bottom = '0';
 
-        floor1.style.animation = 'none' ;
-        floor1.style.right = `${floor1Position}px` ;
+        floor1.style.animation = 'none';
+        floor1.style.right = `${floor1Position}px`;
 
-        floor2.style.animation = 'none' ;
-        floor2.style.right = `${floor2Position}px` ;
+        floor2.style.animation = 'none';
+        floor2.style.right = `${floor2Position}px`;
 
         sound_die.play();
-        
 
-        clearInterval(loop) ;
+        die = 1;
+        clearInterval(loop);
     }
-    if(passouOIntervalo){
-    score_val.innerHTML =+ score_val.innerHTML + 1;
+    if (passouOIntervalo) {
+        score_val.innerHTML = + score_val.innerHTML + 1;
     }
-    if(+score_val.innerHTML%100 === 0){
-    pointing.play();
+    if (+score_val.innerHTML % 100 === 0) {
+        pointing.play();
     }
     frames = frames + 1;
 }, 10)
